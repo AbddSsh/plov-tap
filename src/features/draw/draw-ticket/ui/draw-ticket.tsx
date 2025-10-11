44fac225a965cec826d4f0f65e6d378a0d423e0b
AbddSsh <abduraimovshakh@gmail.com>
2025-10-11 18:36:39 +0500

    feat: current ticket & referral

--- src/features/draw/draw-ticket/ui/draw-ticket.tsx
+++ src/features/draw/draw-ticket/ui/draw-ticket.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, CheckCircle } from "lucide-react";
 import type { FC } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -15,6 +15,9 @@ export const DrawTicket: FC<IDrawTicketProps> = ({
 }) => {
 	const { t } = useTranslation("draw");
 
+	const user_has_ticket = false;
+	const ticket_id = "TFS-123FF-21";
+
 	// api buy ticket mutation
 	const buyTicket = () => {
 		// if success request revalidate getUserRiceCountQuery
@@ -24,25 +27,49 @@ export const DrawTicket: FC<IDrawTicketProps> = ({
 
 	return (
 		<div className="grid grid-flow-row gap-4 w-full">
-			<div className="flex items-center justify-center gap-2">
-				<ArrowDown className="size-6 text-main-color animate-bounce mt-1" />
-				<p className="text-xs uppercase text-center font-semibold text-font-color">
-					{t("draw_current.draw_ticket.title")}
-				</p>
-				<ArrowDown className="size-6 text-main-color animate-bounce mt-1" />
-			</div>
-			<button
-				className={`flex items-center justify-center gap-2 bg-main-color shadow-2xl w-[90%] mx-auto text-font-color rounded-xl h-12 backdrop-blur-sm disabled:grayscale-25 disabled:opacity-50 disabled:pointer-events-none disabled:animate-none animate-pulse`}
-				disabled={riceCount < ticketPrice}
-				onClick={buyTicket}
-			>
-				<span className="text-xl font-bold">🍚</span>
-				<div className="text-center font-bold text-sm">
-					{riceCount < ticketPrice
-						? `${riceCount.toLocaleString()} / ${ticketPrice.toLocaleString()}`
-						: ticketPrice.toLocaleString()}
+			{user_has_ticket ? (
+				<div className="grid grid-cols-[auto_1fr] items-center justify-items-center gap-2 rounded-xl p-5 bg-second-color/10">
+					<div className="w-fit p-2 rounded-full flex items-center justify-center bg-green-500 text-white">
+						<CheckCircle className="size-8" />
+					</div>
+					<div className="grid grid-flow-row gap-1">
+						<p className="text-base font-semibold text-font-color">
+							{t("draw_current.draw_current_ticket.title")}
+						</p>
+						<p className="text-base font-normal text-main-color">
+							<span className="font-semibold text-font-color">
+								{t(
+									"draw_current.draw_current_ticket.ticket_id"
+								)}
+								:
+							</span>{" "}
+							{ticket_id}
+						</p>
+					</div>
 				</div>
-			</button>
+			) : (
+				<>
+					<div className="flex items-center justify-center gap-2">
+						<ArrowDown className="size-6 text-main-color animate-bounce mt-1" />
+						<p className="text-xs uppercase text-center font-semibold text-font-color">
+							{t("draw_current.draw_ticket.title")}
+						</p>
+						<ArrowDown className="size-6 text-main-color animate-bounce mt-1" />
+					</div>
+					<button
+						className={`flex items-center justify-center gap-2 bg-main-color shadow-2xl w-[90%] mx-auto text-font-color rounded-xl h-12 backdrop-blur-sm disabled:grayscale-25 disabled:opacity-50 disabled:pointer-events-none disabled:animate-none animate-pulse`}
+						disabled={riceCount < ticketPrice}
+						onClick={buyTicket}
+					>
+						<span className="text-xl font-bold">🍚</span>
+						<div className="text-center font-bold text-sm">
+							{riceCount < ticketPrice
+								? `${riceCount.toLocaleString()} / ${ticketPrice.toLocaleString()}`
+								: ticketPrice.toLocaleString()}
+						</div>
+					</button>
+				</>
+			)}
 		</div>
 	);
 };
